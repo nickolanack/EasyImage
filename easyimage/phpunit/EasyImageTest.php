1890b0f2c901d839a9c590bbaefe49887c81e61e
nickolanack <nickblackwell82@gmail.com>
2016-01-21 15:10:49 -0800

    add unit test and sample image

--- easyimage/phpunit/EasyImageTest.php
+++ easyimage/phpunit/EasyImageTest.php
@@ -5,5 +5,10 @@ class SharesTest extends PHPUnit_Framework_TestCase {
     /**
      * @runInSeparateProcess
      */
-    public function testReadImages() {}
+    public function testReadImages() {
+
+    	include_once dirname(__DIR__).'/easyimage.php';
+    	EasyImage::Open(__DIR__.'/[G]_[ImAgE]_WBq_ptd_45j.bmp');
+
+    }
 }
