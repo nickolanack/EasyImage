fc2e5c4935d18b55d8d68da411513131b864d7d5
nickolanack <nickblackwell82@gmail.com>
2016-01-21 15:22:38 -0800

    environment test

--- easyimage/phpunit/EasyImageTest.php
+++ easyimage/phpunit/EasyImageTest.php
@@ -2,10 +2,27 @@
 
 class SharesTest extends PHPUnit_Framework_TestCase {
 
+
+
+	/**
+     * @runInSeparateProcess
+     */
+    public function testEnvironment() {
+
+    	$this->assertTrue(function_exists('gd_info'));
+    	
+    	$this->fail(print_r(gd_info(),true));
+	}
+
+
     /**
      * @runInSeparateProcess
      */
     public function testReadImages() {
+
+
+
+
 
     	include_once dirname(__DIR__).'/easyimage.php';
     	$i=EasyImage::Open(__DIR__.'/[G]_[ImAgE]_WBq_ptd_45j.bmp');
