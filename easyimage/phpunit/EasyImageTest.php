76dc6d5fc59089c8c3cb7b0d4bfde4267b9c0550
nickolanack <nickblackwell82@gmail.com>
2016-01-21 15:17:29 -0800

    syntax

--- easyimage/phpunit/EasyImageTest.php
+++ easyimage/phpunit/EasyImageTest.php
@@ -10,7 +10,7 @@ class SharesTest extends PHPUnit_Framework_TestCase {
     	include_once dirname(__DIR__).'/easyimage.php';
     	$i=EasyImage::Open(__DIR__.'/[G]_[ImAgE]_WBq_ptd_45j.bmp');
     	$this->assertEquals(array(
-    		'w'=>945
+    		'w'=>945,
     		'h'=>594
     		), EasyImage::GetSize($i));
 
